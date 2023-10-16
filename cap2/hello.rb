cc766a4d33839bf2d9ff1a0fb64f712126a26d59
K <kogaemi@Macintosh.local>
2023-10-16 22:28:23 +0900

    コミットメッセージ

---
+++ cap2/hello.rb
@@ -0,0 +1,2 @@
+puts "helo world"
+puts "こんにちは"
---
+++ cap2/question2.rb
@@ -0,0 +1,34 @@
+#2-1 1
+puts 2 + 3
+
+#2-1 2
+puts 2 * 2 * 3.14
+
+#2-2 1
+puts "Ruby"
+
+#2-2 2
+puts "abc" + "def"
+
+#2-2 3
+puts "123".to_i + "456".to_i
+
+#2-3 1
+coffee = 300
+espresso = 100
+puts "コーヒー: #{coffee}円"
+puts "合計: #{coffee + espresso * 2}円"
+
+#2-3 2
+coffee = 400
+espresso = 100
+puts "コーヒー: #{coffee}円"
+puts "合計: #{coffee + espresso * 2}円"
+
+#2-6 1
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+
+
